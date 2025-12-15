d806c5d3137128a29f3868fd8ca3037a65da818b
satish-solera <satishsolera54@gmail.com>
2025-12-15 19:47:28 +0530

    fix: navbar uls margin fixed

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -5,8 +5,9 @@ import { Container } from './container'
 import { Heading } from './ui/heading'
 import { SubHeading } from './ui/subHeading'
 import { Button } from './ui/button'
-import { motion } from 'framer-motion'
+import {motion} from 'motion/react'
 import Image from 'next/image'
+
 
 import brainstorm from '../../public/brainstorm.png'
 export const Hero = () => {
@@ -17,7 +18,7 @@ export const Hero = () => {
                     <Heading as='h2' >
                         Take ideas from <br /> better to best
                     </Heading>
-                    <SubHeading as='h3' className='py-4'>
+                    <SubHeading as='h4' className='py-4'>
                         Miro is your teams's visual platform to connect, <br />
                         colloborate, and create - together.
                     </SubHeading>
@@ -27,13 +28,13 @@ export const Hero = () => {
                     </Button>
 
                  
-                    <div className="flex gap-6 items-center bg-gray-200/80  max-w-[300px] rounded-sm mt-10 px-2">
+                    <div className="flex gap-6 itemes-center bg-gray-200/80  max-w-[300px] rounded-sm mt-5 px-3">
                         <div className="flex flex-col "
 
 
                         >
-                            <span>start</span>
-                            <span className='text-xs '>Based on 5149+ reviews</span>
+                            <span className='text-[20px]'>start</span>
+                            <span className='text-[10px] '>Based on 5149+ reviews</span>
                         </div>
 
                         <SubHeading as='h4' className='font-bold'>
@@ -47,9 +48,24 @@ export const Hero = () => {
                 </div>
 
                 <div className=" flex flex-col ">
-                    <div className="md:w-lg md:h-60 w-[250px] h-44 mx-auto md:mx-0 mt-0 ">
+                    <motion.div 
+
+                    initial={{
+                        opacity:0 , x: 20
+                    }}
+
+                    whileInView={{
+                        opacity:1 , x: 0
+                    }}
+
+                    transition={{
+                        duration: 0.4
+                    }}
+
+                    
+                    className="md:w-lg md:h-60 w-[250px] h-44 mx-auto md:mx-0 mt-0 ">
                      <Image src={brainstorm} alt='brain storm hero image' />
-                    </div>
+                    </motion.div>
                     {/* <div className="md:w-lg md:h-16  py-2 grid grid-cols-1 md:grid-cols-3 gap-1 items-center "
 
                     >
